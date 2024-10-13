0b2c61f77055d777408fd43735904102e3293f16
Rifandye <rilfransyah@gmail.com>
2024-10-13 18:35:48 +0700

    chores: improve the database management

--- src/models/category.js
+++ src/models/category.js
@@ -27,6 +27,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'Category',
+      timestamps: true,
     },
   );
   return Category;
--- src/models/merchandise.js
+++ src/models/merchandise.js
@@ -33,6 +33,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'Merchandise',
+      timestamps: true,
     },
   );
   return Merchandise;
